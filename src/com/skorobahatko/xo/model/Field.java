65ea589944fbd1e97e83b0d9f6ee13a5fc0a591e
Stanislav <skorobagatko@mail.com>
2017-08-21 14:28:59 +0300

    Field - add point validation

--- src/com/skorobahatko/xo/model/Field.java
+++ src/com/skorobahatko/xo/model/Field.java
@@ -1,8 +1,15 @@
 package com.skorobahatko.xo.model;
 
+import com.skorobahatko.xo.model.exceptions.AlreadyOccupiedException;
+import com.skorobahatko.xo.model.exceptions.InvalidPointException;
+
 public class Field {
 	
 	private static final int FIELD_SIZE = 3;
+	
+	private static final int MIN_COORDINATE = 0;
+	
+	private static final int MAX_COORDINATE = FIELD_SIZE;
 	
 	private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 	
@@ -12,12 +19,29 @@ public class Field {
 		return FIELD_SIZE;
 	}
 	
-	public Figure getFigure(final Point point) {
+	public Figure getFigure(final Point point) throws InvalidPointException {
+		if (!checkPoint(point)) {
+			throw new InvalidPointException();
+		}
 		return field[point.getX()][point.getY()];
 	}
 	
-	public void setFigure(final Point point, final Figure figure) {
+	public void setFigure(final Point point, final Figure figure) throws InvalidPointException, AlreadyOccupiedException {
+		if (!checkPoint(point)) {
+			throw new InvalidPointException();
+		}
+		if (getFigure(point) != null) {
+			throw new AlreadyOccupiedException();
+		}
 		field[point.getX()][point.getY()] = figure;
+	}
+	
+	private boolean checkPoint(final Point point) {
+		return checkCoordinate(point.getX()) && checkCoordinate(point.getY());
+	}
+	
+	private boolean checkCoordinate(final int coordinate) {
+		return (coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE);
 	}
 
 }
