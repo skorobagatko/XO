24911174d3ace23cbfd01ac7dd06832b60ec3ca1
Stanislav <skorobagatko@mail.com>
2017-08-21 19:42:17 +0300

    MoveController added

---
+++ src/com/skorobahatko/xo/controllers/MoveController.java
@@ -0,0 +1,21 @@
+package com.skorobahatko.xo.controllers;
+
+import com.skorobahatko.xo.model.Field;
+import com.skorobahatko.xo.model.Figure;
+import com.skorobahatko.xo.model.Point;
+import com.skorobahatko.xo.model.exceptions.AlreadyOccupiedException;
+import com.skorobahatko.xo.model.exceptions.InvalidPointException;
+
+public class MoveController {
+	
+	public void applyFigure(final Field field, 
+							final Figure figure, 
+							final Point point) throws InvalidPointException,
+													  AlreadyOccupiedException {
+		if (field.getFigure(point) != null) {
+			throw new AlreadyOccupiedException();
+		}
+		field.setFigure(point, figure);
+	}
+
+}
